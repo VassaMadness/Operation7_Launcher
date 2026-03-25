638e1788d6c8a8670c8038f187cb189ef599ab1a
VassaMadness <95583953+VassaMadness@users.noreply.github.com>
2026-03-25 06:07:12 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,359 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.LabelMover = New System.Windows.Forms.Label()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.wb = New System.Windows.Forms.WebBrowser()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BuscadorDeIDsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FlowAntraxToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'LabelMover
+        '
+        Me.LabelMover.BackColor = System.Drawing.Color.Transparent
+        Me.LabelMover.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.LabelMover.Location = New System.Drawing.Point(419, 98)
+        Me.LabelMover.Name = "LabelMover"
+        Me.LabelMover.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.LabelMover.Size = New System.Drawing.Size(117, 15)
+        Me.LabelMover.TabIndex = 0
+        Me.LabelMover.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ToolTip1
+        '
+        Me.ToolTip1.ToolTipTitle = "FlowAntrax"
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.AllowNavigation = False
+        Me.WebBrowser1.AllowWebBrowserDrop = False
+        Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
+        Me.WebBrowser1.Location = New System.Drawing.Point(4, 121)
+        Me.WebBrowser1.Margin = New System.Windows.Forms.Padding(0, 3, 3, 3)
+        Me.WebBrowser1.MaximumSize = New System.Drawing.Size(634, 448)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(634, 448)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(634, 448)
+        Me.WebBrowser1.TabIndex = 1
+        Me.WebBrowser1.TabStop = False
+        Me.WebBrowser1.Url = New System.Uri("http://launchers.axeso5.com/operation7/launcher.aspx", System.UriKind.Absolute)
+        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(481, 587)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 42)
+        Me.Label2.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AllowDrop = True
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Tahoma", 10.32!)
+        Me.TextBox1.Location = New System.Drawing.Point(420, 11)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(115, 17)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.WordWrap = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.AllowDrop = True
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Font = New System.Drawing.Font("Tahoma", 9.35!)
+        Me.TextBox2.Location = New System.Drawing.Point(421, 40)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(115, 16)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.UseSystemPasswordChar = True
+        Me.TextBox2.WordWrap = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(646, 89)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(274, 20)
+        Me.TextBox3.TabIndex = 7
+        Me.TextBox3.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.Transparent
+        Me.Button1.Location = New System.Drawing.Point(420, 63)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(116, 29)
+        Me.Button1.TabIndex = 8
+        Me.Button1.UseMnemonic = False
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(646, 7)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox1.TabIndex = 9
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.CheckBox1.Visible = False
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(733, 7)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox2.TabIndex = 10
+        Me.CheckBox2.Text = "CheckBox2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        Me.CheckBox2.Visible = False
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(820, 7)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox3.TabIndex = 11
+        Me.CheckBox3.Text = "CheckBox3"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        Me.CheckBox3.Visible = False
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(907, 7)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox4.TabIndex = 12
+        Me.CheckBox4.Text = "CheckBox4"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        Me.CheckBox4.Visible = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(941, 89)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(98, 20)
+        Me.TextBox4.TabIndex = 14
+        Me.TextBox4.Visible = False
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(645, 65)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(234, 20)
+        Me.TextBox5.TabIndex = 15
+        Me.TextBox5.Text = "C:\Axeso5\Operation7\SmartUpdate.exe INET "
+        Me.TextBox5.Visible = False
+        '
+        'wb
+        '
+        Me.wb.Location = New System.Drawing.Point(657, 583)
+        Me.wb.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.wb.Name = "wb"
+        Me.wb.ScriptErrorsSuppressed = True
+        Me.wb.ScrollBarsEnabled = False
+        Me.wb.Size = New System.Drawing.Size(362, 43)
+        Me.wb.TabIndex = 16
+        Me.wb.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Location = New System.Drawing.Point(553, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label1.Size = New System.Drawing.Size(75, 43)
+        Me.Label1.TabIndex = 17
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.NotifyIcon1.BalloonTipText = "Se Ah Iniciado Correctamente"
+        Me.NotifyIcon1.BalloonTipTitle = "$safeprojectname$"
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Tag = "FlowAntrax"
+        Me.NotifyIcon1.Text = "$safeprojectname$"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SalirToolStripMenuItem, Me.AbrirToolStripMenuItem, Me.ToolStripSeparator1, Me.ToolStripMenuItem1})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(146, 76)
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(145, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        Me.SalirToolStripMenuItem.ToolTipText = "Sirve Para Cerrar La Aplicacion."
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(145, 22)
+        Me.AbrirToolStripMenuItem.Text = "Abrir"
+        Me.AbrirToolStripMenuItem.ToolTipText = "Sirve Para Abrir La Aplicacion."
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(142, 6)
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BuscadorDeIDsToolStripMenuItem, Me.FlowAntraxToolStripMenuItem})
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(145, 22)
+        Me.ToolStripMenuItem1.Text = "Herramientas"
+        Me.ToolStripMenuItem1.ToolTipText = "Aqui Encontraras Una Serie De Herramientas"
+        '
+        'BuscadorDeIDsToolStripMenuItem
+        '
+        Me.BuscadorDeIDsToolStripMenuItem.Name = "BuscadorDeIDsToolStripMenuItem"
+        Me.BuscadorDeIDsToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.BuscadorDeIDsToolStripMenuItem.Text = "Buscador De IDs"
+        '
+        'FlowAntraxToolStripMenuItem
+        '
+        Me.FlowAntraxToolStripMenuItem.Name = "FlowAntraxToolStripMenuItem"
+        Me.FlowAntraxToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.FlowAntraxToolStripMenuItem.Text = "FlowAntrax"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(641, 638)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.wb)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.LabelMover)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(641, 638)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(641, 638)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "zLauncher"
+        Me.ToolTip1.SetToolTip(Me, "F" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "l" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "o" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "w" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "A" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "n" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "t" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "r" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "a" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "x")
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LabelMover As System.Windows.Forms.Label
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox4 As System.Windows.Forms.CheckBox
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents wb As System.Windows.Forms.WebBrowser
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents SalirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AbrirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BuscadorDeIDsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FlowAntraxToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,143 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Imports System.IO
+
+#Region "Info/Comments"
+' Este Programa Fue Escrito Para ser usando como Base del Launcher Original de Axeso5 Para Ingresar Al Operation7
+' Esta Recreacion es Basada En la original (Puede Ser Utilizado Como Base Cambiando Unas Cuantas Lineas.
+
+' Discord: "VassaMadness o Vassa Madness#7824" - (Dudas, Sugerencias, Actualizaciones).
+
+' ¿Porque Se Publico?
+' R: Para Los Curiosos, La exploracion del funcionamiento De Un Launcher, Para Los Que Quieran Actualizarlo, Para Los Que Busquen Aprender.
+' Se Publica Al Aver Concluido El Proyecto zXXXXX, Se Agregaron Mejoras A Este Launcher Que Originalmente Faltaban En El Original De Axeso5 (Guardado Automatico de Cuentas, Ayudantes, )
+#End Region
+
+Public Class Form1
+    Dim Datos As String = Application.StartupPath & "\settings.cfg" 'Se Declara la funcion Guardado y Ruta. (Originalmente No Existe En Ax5)
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As Integer) As Integer
+    Private Function MakeHash(ByVal input As String) As String 'Convertimos Los Datos Al Codigo HASH de la web, Necesario Actualizar Para Funcionar Hoy en dia. (Mismo que CRC)
+        Dim a As MD5 = MD5.Create()
+        Dim b As Byte() = a.ComputeHash(Encoding.Default.GetBytes(input))
+        Dim c As New StringBuilder()
+        Dim i As Integer
+
+        For i = 0 To b.Length - 1
+            c.Append(b(i).ToString("x2"))
+        Next i
+        Return c.ToString()
+    End Function
+
+    <Runtime.InteropServices.DllImport("wininet.dll", SetLastError:=True)> _
+    Private Shared Function InternetSetOption(ByVal hInternet As IntPtr, ByVal dwOption As Integer, ByVal lpBuffer As IntPtr, ByVal lpdwBufferLength As Integer) As Boolean
+    End Function
+    Private Sub LabelMover_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LabelMover.Click
+        Process.Start("http://www.axeso5.com/registro")
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+        Me.Close()
+        'Form4.Close() 'Formulario Principal
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        'Funcion Escritura Tipo Texto De La Informacion Perteneciente A Usuario & Contraseña
+        My.Computer.FileSystem.WriteAllText(Datos, "[LOGIN]" & vbCrLf & TextBox1.Text & vbCrLf & TextBox2.Text, False)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try 'Intentamos la lectura de los archivos escritos, Usuario & Contraseña
+            TextBox1.Text = File.ReadLines(Datos)(1)
+            TextBox2.Text = File.ReadLines(Datos)(2)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        ' Timer Main, Registra Lo Realizado En Tiempo Real Del Programa
+
+        Me.Text = "zClauncher - " + TimeOfDay
+        TextBox3.Text = "&password=" & MakeHash(TextBox2.Text)
+
+        Dim HK1 As Boolean
+        HK1 = GetAsyncKeyState(Keys.Enter)
+        If HK1 = True Then
+            Button1.Show()
+            Button1.Select()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        'Usuario'
+        If TextBox1.Text = "" Then
+            Fail.Show()
+        Else
+            CheckBox1.Checked = True
+        End If
+
+        'Contraseña'
+        If TextBox2.Text = "" Then
+            Fail.Show()
+        Else
+            CheckBox1.Checked = True
+        End If
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.CheckState = CheckState.Checked Then
+            wb.Navigate("http://209.251.184.23/services/op7auth.ashx?loginid=" & TextBox1.Text + TextBox3.Text) 'Antigua Auth En Axeso5, Es Necesario Actualizar.
+            Timer2.Enabled = True
+            CheckBox1.Checked = False
+        Else
+            CheckBox1.Checked = False
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        'Filtrado HTML
+        Try
+            Dim MIIP As String = wb.DocumentText
+            MIIP = MIIP.Remove(0, MIIP.IndexOf("otp=" & "") + 4)
+            TextBox4.Text = MIIP.Substring(0, MIIP.IndexOf("<"))
+        Catch ex As Exception
+        End Try
+        CheckBox2.Checked = True
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox2.CheckedChanged
+        Dim A As String
+        A = Shell(TextBox5.Text + TextBox1.Text + " " + TextBox4.Text, AppWinStyle.NormalFocus) 'Llamada Al Juego Con Verificacion Web
+        Me.Close()
+        'Form3.Show() 'Funcion Extra Buscar (Obsoleta y eliminada) - Buscador De IDs
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+        MsgBox("Ups!! Parece Que El Teclado No Esta Disponible.", MsgBoxStyle.AbortRetryIgnore, "Error En El Teclado")
+    End Sub
+
+    Private Sub AbrirToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AbrirToolStripMenuItem.Click
+        Me.Show()
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SalirToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    'Funcion Extra Buscar (Obsoleta y eliminada) - Buscador De IDs
+    Private Sub BuscadorDeIDsToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BuscadorDeIDsToolStripMenuItem.Click
+        'Form3.Show()
+    End Sub
+
+    'Funcion extra minimizar el archivo
+    Private Sub CheckBox2_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles CheckBox2.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+            NotifyIcon1.BalloonTipText = "El Programa Se Ah Minimizado"
+            NotifyIcon1.BalloonTipTitle = "Launcher v1.0"
+            NotifyIcon1.Visible = True
+            NotifyIcon1.ShowBalloonTip(0)
+        End If
+    End Sub
+End Class
